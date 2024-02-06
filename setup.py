a976f14f0d3f297b4f7cd4f2c1cdec5b17cc01fb
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-02-06 13:22:05 -0500

    issue #26: typo in setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        "requests"
+        'requests'
     ],
 )
