91bccd1dedf8bf2780fe92a993c0dbad86e5b35d
SonOfLope <jonathan.lopez@inspection.gc.ca>
2024-09-19 10:25:51 -0400

    Adds missing steps to apply schema for custom postgres image

--- dockerfiles/postgres/entrypoint.sh
+++ dockerfiles/postgres/entrypoint.sh
@@ -2,4 +2,10 @@
 
 python3 /app/fetch_schema.py
 
+pg_ctl -D "/var/lib/postgresql/data" -o "-c listen_addresses=''" -w start
+
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /app/schema.sql
+
+pg_ctl -D "/var/lib/postgresql/data" -m fast -w stop
+
 exec docker-entrypoint.sh "$@"
