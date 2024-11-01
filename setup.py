fb6d5e27b72f3af395491fe4ea5308ee1a4d8d84
ThomasCardin <tomcardin@outlook.com>
2024-11-01 09:04:19 -0400

    issue #26: added webtop-template to the python package

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     entry_points={
         'console_scripts': [
             'remove-previous-images=remove_previous_image.remove_previous_image:main',
+            'webtop-template=webtop-template.main:main',
         ],
     },
     url='https://github.com/ai-cfia/devops.git',
